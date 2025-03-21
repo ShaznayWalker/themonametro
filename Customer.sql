28584fac05a87a644d3781191ea21d0ec111f8a2
skillhacker18 <71279301+skillhacker18@users.noreply.github.com>
2025-03-21 08:48:03 -0500

    Update Customer.sql

--- Customer.sql
+++ Customer.sql
@@ -4,9 +4,10 @@ USE Customer;
 DROP table if exists users;
 
 CREATE TABLE users (
-	id int auto_increment primary key,
+    id int auto_increment primary key,
     Firstname varchar(100),
     Lastname varchar(100),
+    username varchar(100),
     email varchar(100) unique,
     password varchar(255)
 );
